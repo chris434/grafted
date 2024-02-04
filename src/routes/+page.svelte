ec79f725ffdfc1312826e15c93d95736c88a9454
chris434 <christopherpearce589@gmail.com>
2024-02-04 12:22:21 +0000

    feat: tree view container

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import TreeView from "../views/treeView.svelte";
+
+</script>
+<TreeView/>
---
+++ src/views/treeView.svelte
@@ -0,0 +1,5 @@
+<div class="flex justify-center w-full h-[80%]">
+ <main class="lg:w-6/12 md:w-8/12 sm:w-9/12 w-full  border-2 border-green-500 m-5">
+
+</main>
+</div>
