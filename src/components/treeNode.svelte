207f1dc447b7482a8e8d1d15dd7b423a6c555595
chris434 <christopherpearce589@gmail.com>
2024-03-03 08:22:23 +0000

    feat: node

---
+++ src/components/treeNode.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type {Node } from "../types/treeTypes";
+
+    export let node:Node
+    let selected=false
+    const ICONS={'false':'chevron-up','true':'chevron-down'} as const
+
+    function selectNode(){
+        selected =!selected
+    }
+</script>
+
+<li>
+<button on:click={selectNode} class="p-3 w-full text-2xl flex gap-3 items-center {selected?'bg-green-400 text-white':''}">
+ <iconify-icon icon="mdi:{ICONS[`${selected}`]}"></iconify-icon>
+    <div>{node.label}</div>  
+</button>
+</li>
--- src/views/treeView.svelte
+++ src/views/treeView.svelte
@@ -1,15 +1,17 @@
 <script lang='ts'>
 	import ActionBar from "../components/actionBar.svelte";
-	import TextField from "../components/textField.svelte";
 	import TextForm from "../components/textForm.svelte";
 	import ToolBar from "../components/toolBar.svelte";
+	import TreeNode from "../components/treeNode.svelte";
 	import {getTrees}from '../contexts/treeContext'
 	import { getTreeIndex } from "../contexts/treeIndexContext";
 	const trees=getTrees()
 	const {updateName}=trees
 	const treeIndex=getTreeIndex()
 
-	let value =$trees[treeIndex]?.name
+	$: tree=$trees[treeIndex]
+	$: value =tree?.name
+	$: root=tree?.root
 	function updateNameHandler(value:string){
 		const hasName=updateName(value,treeIndex)
 		if(hasName) return 'name is already taken'
@@ -23,16 +25,16 @@
 <TextForm submitCb={updateNameHandler} nullHandler='fallback' placeholder="name" {value}/>
 </ToolBar>
 
-{#if $trees[0]}
+
 <div class="flex justify-center w-full h-[80%]">
  <main class="lg:w-6/12 md:w-8/12 sm:w-9/12 w-full  border-2 border-green-500 m-5">
 <ActionBar/>
-</main>
-
-</div>
-{:else}
-  <svg class="animate-spin h-6/12 w-6/12 rounded-full mr-3 bg-green-400" viewBox="0 0 24 24">
-    <!-- ... -->
-  </svg>
+{#if root}
+<ul>
+<TreeNode node={root}/>
+</ul>
 {/if}
+</main>
+</div>
+
 
